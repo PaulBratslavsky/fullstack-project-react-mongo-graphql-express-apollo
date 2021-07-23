f73d20a48fac7846b997d0520031e394c658625b
Paul Bratslavsky <Paul.bratslavsky@gmail.com>
2021-07-22 21:04:23 -0500

    added back end sign in auth mutation with graphql

--- server/graphql/resolvers/mutations.js
+++ server/graphql/resolvers/mutations.js
@@ -1,30 +1,60 @@
-const { User } = require('../../models/user')
-const { UserInputError, AuthenticationError, ApolloError } = require('apollo-server-express')
+const { User } = require("../../models/user");
+const {
+  UserInputError,
+  AuthenticationError,
+  ApolloError,
+} = require("apollo-server-express");
 
 module.exports = {
   Mutation: {
-
     signUp: async (parent, args, context, info) => {
       try {
         const user = new User({
           email: args.fields.email,
-          password: args.fields.password
-        })
-        
-        const token = await user.generateToken()
-        if(!token) throw new AuthenticationError('Server Error')
-        return { ...token._doc }
+          password: args.fields.password,
+        });
+
+        const token = await user.generateToken();
+        if (!token) throw new AuthenticationError("Server Error");
+        return { ...token._doc };
       } catch (error) {
         if (error.code === 11000) {
-          throw new AuthenticationError('Duplicate Entry')
+          throw new AuthenticationError("Duplicate Entry");
         }
-        throw error
-      } 
+        throw error;
+      }
     },
 
     signIn: async (parent, args, context, info) => {
-      return true
-    }
+      // 1. get user
+      const user = await User.findOne({ email: args.fields.email });
 
-  }
-}
+      if (!user) {
+        throw new AuthenticationError("Please provide valid user email");
+      }
+
+      // 2. check password
+      const password = await user.checkPassword(args.fields.password);
+
+      if (!password) {
+        throw new AuthenticationError("Please provide valid password");
+      }
+
+      // 3. generate token
+      const token = await user.generateToken();
+
+      if (!token) {
+        throw new AuthenticationError("Server Error");
+      }
+
+      return {
+        _id:user._id,
+        firstName:user.firstName,
+        lastName:user.lastName,
+        email:user.email,
+        token:user.token,
+      };
+
+    },
+  },
+};
--- server/models/user.js
+++ server/models/user.js
@@ -1,70 +1,72 @@
-const mongoose = require('mongoose')
-const validator = require('validator')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-require('dotenv').config()
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const UserSchema = mongoose.Schema({
-
-  email:{
-    type:String,
-    required:true,
-    trim:true,
-    unique:true,
-    lowercase:true,
-    validate:[validator.isEmail, 'invalid email']
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, "invalid email"],
   },
 
-  password:{
-    type:String,
-    required:true,
-    minlength:5,
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
   },
 
   firstName: {
-    type:String,
+    type: String,
     maxlength: 50,
   },
 
   lastName: {
-    type:String,
+    type: String,
     maxlength: 50,
   },
 
   token: {
-    type:String
-  }
-})
+    type: String,
+  },
+});
 
-UserSchema.pre('save', function(next) {
-  var user = this
-  if (user.isModified('password')) {
+UserSchema.pre("save", function (next) {
+  var user = this;
+  if (user.isModified("password")) {
+    bcrypt.genSalt(10, function (error, salt) {
+      if (error) return next(error);
 
-    bcrypt.genSalt(10, function(error, salt) {
-      if (error) return next(error)
-
-      bcrypt.hash(user.password, salt, function(error,hash) {
-        if (error) return next(error)
-        user.password = hash
-        next()
-      })
-    })
+      bcrypt.hash(user.password, salt, function (error, hash) {
+        if (error) return next(error);
+        user.password = hash;
+        next();
+      });
+    });
   } else {
-    next()
+    next();
   }
-})
+});
 
-UserSchema.methods.generateToken = async function() {
-  var user = this
-  var token = jwt.sign(
-    {email:user.email},
-    process.env.SECRET, 
-    {expiresIn:'7d'}
-  )
+UserSchema.methods.checkPassword = async function (candidatePassword) {
+  var user = this;
+  const result = await bcrypt.compare(candidatePassword, user.password);
+  return result;
+};
+
+UserSchema.methods.generateToken = async function () {
+  var user = this;
+  var token = jwt.sign({ email: user.email }, process.env.SECRET, {
+    expiresIn: "7d",
+  });
   user.token = token;
-  return user.save()  
-}
+  return user.save();
+};
 
-const User = mongoose.model('User', UserSchema)
+const User = mongoose.model("User", UserSchema);
 
-module.exports = { User }
+module.exports = { User };
